589f99fdcab9a5f68042763354c37e62e5055db3
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2022-01-09 18:38:06 +1100

    change database extension to sqlite3

--- create-db.py
+++ create-db.py
@@ -1,13 +1,15 @@
 import sqlite3
 
-con = sqlite3.connect('villager-trade-tracker.db')
+con = sqlite3.connect('villager-trade-tracker.sqlite3')
 cur = con.cursor()
 
 cur.execute('''CREATE TABLE villagers
-				(id, name, type, level, user)''')
+				(id, name, type, level, user);''')
+
+con.commit()
 
 cur.execute('''CREATE TABLE users
-				(id, username, 2facode)''')
+				(id, username)''')
 
 con.commit()
 
