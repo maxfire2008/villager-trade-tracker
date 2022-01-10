e415c24a0d73ad1c419492c1af87e0ec598bea40
maxrichardburgess <22722054+maxfire2008@users.noreply.github.com>
2022-01-10 14:38:27 +1100

    add enchanted books

--- enchantment_importer.py
+++ enchantment_importer.py
@@ -1,5 +1,5 @@
 table_file = input("Input table txt file from https://minecraft.fandom.com/wiki/Enchanting#Summary_of_enchantments\n>")
-table = open(table_file,"rb").read().decode()
+table = open(table_file).read()
 
 roman_numerals = {
 	"i": 1,
@@ -13,4 +13,10 @@ roman_numerals = {
 for row in table.split("\n"):
 	if "soul speed" in row.lower():
 		print("Skipped",row,"as villagers don't sell soul speed'")
-	print(row.split("\t"))
+	else:
+		row_split=row.split("\t")
+		if len(row_split) > 1 and roman_numerals[row_split[4].lower()] > 1:
+			for book_num in range(roman_numerals[row_split[4].lower()]):
+				print("book_of_"+row_split[0].lower().replace(" ","_")+"_"+str(book_num+1))
+		else:
+			print("book_of_"+row_split[0].lower().replace(" ","_"))
